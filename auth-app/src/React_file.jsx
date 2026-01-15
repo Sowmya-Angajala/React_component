07fbadf254046d7bc307e61eaa05743335ba5842
Sowmya-Angajala <angajalasowmya02@gmail.com>
2026-01-15 10:57:07 +0530

    File uploaded is done

--- auth-app/src/React_file.jsx
+++ auth-app/src/React_file.jsx
@@ -3,7 +3,7 @@ import React ,{useState} from "react";
 function FileUpload() {
     const [file,setFile] =useState(null);
     const [message,setMessage] =useState("");
-    const [loadinf,setLoading] =useState(false);
+    const [loading,setLoading] =useState(false);
 
     const handleFIlechange =(e) =>{
         setFile(e.target.files[0]);
@@ -12,7 +12,46 @@ function FileUpload() {
     const handleSubmit =async (e) =>{
         e.prevendDefault()
         if(!file){
-            setMessage("Please ")
+            setMessage("Please  select a file")
+            return
+        }
+        const formData =mew FormData();
+        formData.append("file",file);
+        try{
+            setLoading(true);
+            setMessage("");
+
+            const response =await fetch("https://example.com/upload",{
+                method:"POST ",
+                body:formData,
+
+            });
+            if(!response.ok){
+                throw new Error("upload failed")
+            }
+            setMessage("File uploaded successfully");
+        }
+        catch(error){
+            setMessage("Error uploading file")
+        }
+        finally{
+            setMessage(false);
         }
     }
+    return(
+        <div>
+            <h3>
+                Upload File
+            </h3>
+            <form onSubmit={handleSubmit}>
+                <input type="file" 
+                onChange={handleFIlechange}/>
+                <button type="submit" disabled={loading}>
+                    {loading ? "Uploading..." : "Upload"}
+                </button>
+            </form>
+            {message && <P>{message}</P>}
+        </div>
+    )
 }
+export default FileUpload
